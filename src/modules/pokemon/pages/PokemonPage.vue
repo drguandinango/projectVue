191c5dec72fb2630949136308a645455d90f492d
drguandinango <drguandinango@pucesi.edu.ec>
2026-06-10 07:53:21 -0500

    VUE pokemon +IA+COOLOR CONTADOR

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,16 +1,15 @@
 <template>
     <section class="flex flex-col">
-        <h1>Pokémon <small class="text-blue-500">#{id}</small></h1>
+        <h1 class="text-3xl font-bold">Pokémon <small class="text-blue-500">#{{ id }}</small></h1>
 
 
-        <img :src="pokemonImageUrl"
-            alt="pokemon" class="w-[200px] h-[200px] mt-5" width="200" height="200" />
+        <img :src="pokemonImageUrl" alt="pokemon" class="w-[200px] h-[200px] mt-5" width="200" height="200" />
 
         <RouterLink class="
 bg-blue-500
 text-white
-p-2 rounded mt-5
-text-center" :to="{ name: 'pokemon', params: { id: nextPokemonId } }">suguiente</RouterLink>
+p-2 rounded mt-5 text-center hover:bg-blue-700" :to="{ name: 'pokemon', params: { id: nextPokemonId } }">suguiente
+        </RouterLink>
 
     </section>
 </template>
